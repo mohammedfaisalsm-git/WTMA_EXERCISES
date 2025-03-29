7749d1f7f5dc4056cf9ff2251466f2b0e525289f
mohammedfaisalsm-git <231801105@rajalakshmi.edu.in>
2025-03-29 11:47:16 +0530

    Create Signin.java

---
+++ src/main/java/com.faisalSM.servlet/Signin.java
@@ -0,0 +1,55 @@
+package com.faisalSM.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Signin
+ */
+@WebServlet("/Signin")
+public class Signin extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Signin() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		String a=request.getParameter("name");
+		String b=request.getParameter("pass");
+		String user="Faisal";
+		String pass="1234";
+		PrintWriter p=response.getWriter();
+		if(a.equals(user) && b.equals(pass)) {
+			p.print("Login successfully...");
+		}
+		else {
+			p.print("Login failed...");
+		}
+			
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
